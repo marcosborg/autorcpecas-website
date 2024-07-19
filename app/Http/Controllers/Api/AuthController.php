dab33cb60e862acbd85de5322b1d3f2afb0b8e9c
marcosborg <m.borges.mail@gmail.com>
2024-07-19 10:26:19 +0100

    Update

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        $credentials = request(['email', 'password']);
+        if (!auth()->attempt($credentials)) {
+            return response()->json([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'password' => [
+                        'Invalid credentials'
+                    ],
+                ]
+            ], 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+        $authToken = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $authToken,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,6 @@
 <?php
 
+Route::post('login', 'Api\\AuthController@login');
+
 Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
 });
